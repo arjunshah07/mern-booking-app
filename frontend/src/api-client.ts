90dd096ef9b1d3ade98658dd940f40192db18078
Arjun <arjunshahcool@gmail.com>
2024-07-26 15:52:38 +0530

    fixes api

--- frontend/src/api-client.ts
+++ frontend/src/api-client.ts
@@ -1,6 +1,6 @@
 import {RegisterFormData} from "./pages/Register";
 import { SignInFormData } from "./pages/Signin";
-import { HotelType , HotelSearchResponse } from "../../backend/src/shared/types";
+import { HotelType , HotelSearchRespose} from "../../backend/src/shared/types";
 
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "";
 export const register = async (formData : RegisterFormData) =>{
